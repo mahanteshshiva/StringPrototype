abb748043263036734fa93c6d448a0d643b25524
Mahantesh <mahantesh0112@gmail.com>
2019-10-28 12:23:00 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var x = "safe and secure payments"
+console.log("length of the string is " + x.length);
+//  returns length of the string 
+
+var x = "Hello world"
+console.log("character at index 4 is " + " " + x.charAt(6));
+// returns the character  
+
+var x = "Hello world";
+console.log("character code at 3 is " + x.charCodeAt(3));
+//returns the character code 
+
+var x = "$%(&";
+console.log("character point at 3 " + x.codePointAt(3));
+//returns the character point 
+
+var x = "Hello";
+var y= "world";
+console.log("combining two string "  + x.concat(" " , y));
+
+var x = "Hello mahantesh welcome  my world";
+console.log(x.includes("to"));
+
+var x = "cnclnc cnslkcd dcjkdlc cdjclkj"
+console.log(x.indexOf("cnslkcd"));
+
+var x = "cnclnc cnslkcd dcjkdlc cdjclkj";
+console.log(x.lastIndexOf("cnslkcd"));
+
